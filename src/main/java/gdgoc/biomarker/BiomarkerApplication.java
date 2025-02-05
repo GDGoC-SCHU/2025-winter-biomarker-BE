bb2d11f466e17efaeff1ba631823859e0749fc50
giyomdabin <dabinjang01@gmail.com>
2025-02-05 15:35:40 +0900

    init

---
+++ src/main/java/gdgoc/biomarker/BiomarkerApplication.java
@@ -0,0 +1,13 @@
+package gdgoc.biomarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiomarkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiomarkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gdgoc/biomarker/BiomarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package gdgoc.biomarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiomarkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
